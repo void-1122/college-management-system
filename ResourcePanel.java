ed32e8abc42ad20fa245f616f643d7193408f368
void-1122 <vin11224466@gmail.com>
2026-04-01 04:28:14 +0530

    Update ResourcePanel.java

--- ResourcePanel.java
+++ ResourcePanel.java
@@ -1,1 +1,21 @@
-String[][] data = DataStore.resources.toArray(new String[0][]);
+import javax.swing.*;
+import java.awt.*;
+
+public class ResourcePanel extends JPanel {
+
+    public ResourcePanel() {
+
+        setLayout(new BorderLayout());
+
+        JLabel heading = new JLabel("Resources Section");
+        heading.setHorizontalAlignment(JLabel.CENTER);
+
+        String[][] data = DataStore.resources.toArray(new String[0][]);
+        String col[] = {"Title", "Subject"};
+
+        JTable table = new JTable(data, col);
+
+        add(heading, BorderLayout.NORTH);
+        add(new JScrollPane(table), BorderLayout.CENTER);
+    }
+}
