132124439c369bd314dbe766004ee0552baef6c0
void-1122 <vin11224466@gmail.com>
2026-04-01 04:19:40 +0530

    Update QuizPanel.java

--- QuizPanel.java
+++ QuizPanel.java
@@ -1,32 +1,7 @@
-import javax.swing.*;
-import java.awt.*;
+JLabel q = new JLabel(DataStore.questions.get(0));
+String[] opts = DataStore.options.get(0);
 
-public class QuizPanel extends JPanel {
-
-    public QuizPanel() {
-
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
-        JLabel q = new JLabel("Q1: What is Java?");
-
-        JRadioButton a = new JRadioButton("Language");
-        JRadioButton b = new JRadioButton("Database");
-        JRadioButton c = new JRadioButton("OS");
-        JRadioButton d = new JRadioButton("Browser");
-
-        ButtonGroup bg = new ButtonGroup();
-        bg.add(a);
-        bg.add(b);
-        bg.add(c);
-        bg.add(d);
-
-        JButton next = new JButton("Next");
-
-        add(q);
-        add(a);
-        add(b);
-        add(c);
-        add(d);
-        add(next);
-    }
-}
+JRadioButton a = new JRadioButton(opts[0]);
+JRadioButton b = new JRadioButton(opts[1]);
+JRadioButton c = new JRadioButton(opts[2]);
+JRadioButton d = new JRadioButton(opts[3]);
